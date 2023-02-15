51f0b45c04fda99d0935a30892e4c84e377de8c7
ayaabusoud <ayaabusoud23@gmail.com>
2023-02-15 15:25:25 +0200

    Update hotel.js

--- js/hotel.js
+++ js/hotel.js
@@ -5,6 +5,7 @@ bookRoom.forEach(element => {
     element.addEventListener("click",function(event){
         let roomImg = element.parentElement.parentElement.firstElementChild.firstElementChild;
         roomImg.innerHTML += `<div class="overlayRoom"><p class="overlayText">Booked</p></div>`
+        element.remove();
 
     })
 });
